7f4f44c06fdae3f51e34d632ab7ea54936eff656
ticktW <123@com>
2025-11-16 10:52:19 +0800

    prepare dataset

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 pip install -r requirements.txt
-pip install modelscope
+pip install vllm
+# pip install modelscope
 # 安装核心工具（已安装 datasets 库可跳过，CLI 已包含）
 
-modelscope download --dataset gongjy/minimind_dataset pretrain_hq.jsonl --local_dir ./dataset
+# modelscope download --dataset gongjy/minimind_dataset pretrain_hq.jsonl --local_dir ./dataset
+./hfd.sh jingyaogong/minimind_dataset  --dataset --local_dir ./dataset
 
 #检查./dataset下文件是否存在
 if [ ! -f "./dataset/pretrain_hq.jsonl" ]; then
